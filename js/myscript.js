cb846a798416ab245c7bfae5f0da2c56f3ce93a7
Preston Edmands <pedmands@gmail.com>
2015-09-02 14:18:39 -0400

    Added scrollspy, smooth scroll, and fading/full-height carousels.

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,83 @@
-$('.carousel').carousel({
-  interval: false
+$(function(){
+	
+	"use strict";
+
+	var topoffset = 50; //variable for menu height
+	var slideqty = $('#featured .item').length;
+	var wheight = $(window).height(); // get height of window
+
+
+	$('.fullheight').css('height', wheight);
+
+	//replace carousel img with bg image
+	$('#featured .item img').each(function(){
+		var imgSrc = $(this).attr('src');
+		$(this).parent().css({'background-image': 'url('+imgSrc+')'});
+		$(this).remove();
+	});
+
+	//adjust height of fullheight elements on window resize
+	$(window).resize(function(){
+		wheight = $(window).height(); // get height of window
+		$('.fullheight').css('height', wheight);
+	});
+
+	//Activate Scrollspy
+	$('body').scrollspy({
+		target: 'header .navbar',
+		offset: topoffset
+	});
+
+	//add inbody class
+	var hash = $(this).find('li.active a').attr('href');
+		if(hash !== '#featured') {
+			$('header nav').addClass('inbody');
+		} else {
+			$('header nav').removeClass('inbody');
+		}
+
+	// And an inbody class when scrollspy even fires.
+	$('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
+		var hash = $(this).find('li.active a').attr('href');
+		if(hash !== '#featured') {
+			$('header nav').addClass('inbody');
+		} else {
+			$('header nav').removeClass('inbody');
+		}
+	});
+
+//Use smooth scrolling when clicking on navigation
+  	$('.navbar a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') === 
+	      this.pathname.replace(/^\//,'') && 
+	      location.hostname === this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top-topoffset+2
+	        }, 500);
+	        return false;
+	      } //target.length
+	    } //click function
+	 }); //smooth scrolling
+
+  	//Automatically generate carousel indicators
+  	for (var i=0; i < slideqty; i++) {
+	    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
+	    if (i === randSlide) {
+	      insertText += ' class="active" ';
+	    }
+	    insertText += '></li>';
+	    $('#featured ol').append(insertText);
+	}
+
+	$('.carousel').carousel({
+  	interval: false
+	});
+
 });
+
+
+
+
