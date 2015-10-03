b577e0c3c3939c166cff62bb170fbed35867fb1f
Preston Edmands <pedmands@gmail.com>
2015-10-03 06:32:14 -0400

    Final beta commit

--- components/scripts/myscript.js
+++ components/scripts/myscript.js
@@ -132,4 +132,29 @@ $('a[href*=#]:not([href=#])').click(function() {
     interval: 3000
   });
 
+  //google map
+  var styles = [
+  {
+    stylers: [
+      { hue: "#00ffe6" },
+      { saturation: -20 }
+    ]
+  },{
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      { lightness: 100 },
+      { visibility: "simplified" }
+    ]
+  },{
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+map.setOptions({styles: styles});
+
 });
